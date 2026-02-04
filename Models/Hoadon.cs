be301a55ca608b18a3c349c1168381ace09869da
VanLinh4869 <linh_dth225684@student.agu.edu.vn>
2026-02-04 11:13:26 +0700

    Add files via upload

---
+++ Models/Hoadon.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace shop.Models;
+
+[Table("HOADON")]
+public partial class Hoadon
+{
+    [Key]
+    [Column("MaHD")]
+    public int MaHd { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? Ngay { get; set; }
+
+    public int? TongTien { get; set; }
+
+    [Column("MaKH")]
+    public int MaKh { get; set; }
+
+    // 0 = chờ duyệt, 1 = đã duyệt, 2 = đã giao, 3 = đã hủy
+    public int? TrangThai { get; set; }
+
+    [Column("MaNvDuyet")]
+    public int? MaNvDuyet { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? NgayDuyet { get; set; }
+
+    [InverseProperty("MaHdNavigation")]
+    public virtual ICollection<Cthoadon> Cthoadons { get; set; } = new List<Cthoadon>();
+
+    [ForeignKey("MaKh")]
+    [InverseProperty("Hoadons")]
+    public virtual Khachhang MaKhNavigation { get; set; } = null!;
+
+    [ForeignKey("MaNvDuyet")]
+    [InverseProperty("HoadonsDuyet")]
+    public virtual Nhanvien? MaNvDuyetNavigation { get; set; }
+}
---
+++ Models/Nhanvien.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace shop.Models;
+
+[Table("NHANVIEN")]
+public partial class Nhanvien
+{
+    [Key]
+    [Column("MaNV")]
+    public int MaNv { get; set; }
+
+    [StringLength(100)]
+    public string Ten { get; set; } = null!;
+
+    [Column("MaCV")]
+    public int MaCv { get; set; }
+
+    [StringLength(20)]
+    [Unicode(false)]
+    public string? DienThoai { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Email { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? MatKhau { get; set; }
+
+    [ForeignKey("MaCv")]
+    [InverseProperty("Nhanviens")]
+    public virtual Chucvu MaCvNavigation { get; set; } = null!;
+    [InverseProperty("MaNvDuyetNavigation")]
+    public virtual ICollection<Hoadon> HoadonsDuyet { get; set; } = new List<Hoadon>();
+}
